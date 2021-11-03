b310c92b6f40122f7a414ed01abd605a2a7ef8e9
漆伟龙 <qiwl@maycur.com>
2021-11-03 08:48:48 +0800

    chore: 测试

--- src/container/home-page/index.tsx
+++ src/container/home-page/index.tsx
@@ -2,9 +2,34 @@ import * as React from 'react';
 import './index.scss';
 
 function HomePage() {
+  const ref = React.useRef(null);
+  React.useEffect(() => {
+    if (ref.current) {
+      window.addEventListener('keydown', handleKeyDown);
+    }
+    return () => {
+      if (ref.current) {
+        window.removeEventListener('keydown', handleKeyDown);
+      }
+    };
+  }, [ref]);
+
+  const handleKeyDown = React.useCallback((e) => {
+    if ((e.ctrlKey || e.metaKey) && e.keyCode === 67) {
+      // 复制
+      console.log('复制');
+    }
+    if ((e.ctrlKey || e.metaKey) && e.keyCode === 86) {
+      // 粘贴
+      console.log('粘贴');
+    }
+  }, []);
+
   return (
     <div className="home-container">
-      <div className="text-container">欢迎来到我的世界</div>
+      <div className="text-container" ref={ref}>
+        欢迎来到我的世界
+      </div>
       <div className="action-button">点击进入</div>
       <svg width="0" height="0">
         <filter id="displacement-text-filter">
